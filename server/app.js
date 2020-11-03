4ae4900673b5051f15130da75c02e0f033ef7cfa
Steven Cho <sxc6061@g.rit.edu>
2020-11-03 18:57:58 -0500

    Domomaker C complete

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -25,25 +29,56 @@ mongoose.connect(dbURL, moongooseOptions, (err) => {
     }
 });
 
+let redisURL = {
+    hostname: 'redis-13818.c241.us-east-1-4.ec2.cloud.redislabs.com',
+    port: '13818',
+}
+
+let redisPASS = 'ZX5HroH35NRU7V0sdI0eR7Zs28vfZt82';
+if (process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    [, redisPASS] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPASS,
+});
+
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 app.use(session({
     key: 'sessionid',
+    store: new RedisStore({
+        client: redisClient,
+    }),
     secret: 'Domo Arigato',
     resave: true,
     saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+    },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
--- server/router.js
+++ server/router.js
@@ -1,14 +1,15 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
-    app.get('/login', controllers.Account.loginPage);
-    app.post('/login', controllers.Account.login);
-    app.get('/signup', controllers.Account.signupPage);
-    app.post('/signup', controllers.Account.signup);
-    app.get('/logout', controllers.Account.logout);
-    app.get('/maker', controllers.Domo.makerPage);
-    app.post('/maker', controllers.Domo.make);
-    app.get('/', controllers.Account.loginPage);
+    app.get('/login', controllers.Account.loginPage, mid.requiresSecure, mid.requiresLogout);
+    app.post('/login', controllers.Account.login, mid.requiresSecure, mid.requiresLogout);
+    app.get('/signup', controllers.Account.signupPage, mid.requiresSecure, mid.requiresLogout);
+    app.post('/signup', controllers.Account.signup, mid.requiresSecure, mid.requiresLogout);
+    app.get('/logout', controllers.Account.logout, mid.requiresLogin);
+    app.get('/maker', controllers.Domo.makerPage, mid.requiresLogin);
+    app.post('/maker', controllers.Domo.make, mid.requiresLogin);
+    app.get('/', controllers.Account.loginPage, mid.requiresSecure, mid.requiresLogout);
 };
 
 module.exports = router;
