2ac0d4f406c8fa31cd49f863b3e2fb50c3ef6d6a
Steven Cho <sxc6061@g.rit.edu>
2020-11-08 17:11:20 -0500

    domomake d initial commit

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const models = require('../models');
 
 const Domo = models.Domo;
@@ -42,5 +43,20 @@ const makeDomo = (req, res) => {
     return domoPromise;
 };
 
+const getDomos = (request, response) => {
+    const req = request;
+    const res = response;
+
+    return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: 'An error occurred' });
+        }
+
+        return res.json({ domos: docs });
+    });
+};
+
 module.exports.makerPage = makerPage;
 module.exports.make = makeDomo;
+module.exports.getDomos = getDomos;
--- server/router.js
+++ server/router.js
@@ -2,9 +2,10 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
+    app.get('/getToken', controllers.Account.getToken, mid.requiresSecure);
+    app.get('/getDomos', controllers.Domo.getDomos, mid.requiresLogin);
     app.get('/login', controllers.Account.loginPage, mid.requiresSecure, mid.requiresLogout);
     app.post('/login', controllers.Account.login, mid.requiresSecure, mid.requiresLogout);
-    app.get('/signup', controllers.Account.signupPage, mid.requiresSecure, mid.requiresLogout);
     app.post('/signup', controllers.Account.signup, mid.requiresSecure, mid.requiresLogout);
     app.get('/logout', controllers.Account.logout, mid.requiresLogin);
     app.get('/maker', controllers.Domo.makerPage, mid.requiresLogin);
