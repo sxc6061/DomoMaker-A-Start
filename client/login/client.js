23afe99103ac2a8a83b6050a0c30be51de6b353b
Steven Cho <sxc6061@g.rit.edu>
2020-11-12 21:22:21 -0500

    fixed submit button

--- client/login/client.js
+++ client/login/client.js
@@ -49,7 +49,7 @@ const LoginWindow = (props) => {
             <label htmlFor="pass">Password: </label>
             <input id="pass" type="password" name="pass" placeholder="password"/>
             <input type="hidden" name="_csrf" value={props.csrf}/>
-            <input className="formSubmit" tpye="submit" value="Sign in"/>
+            <input className="formSubmit" type="submit" value="Sign in"/>
         </form>
     );
 };
@@ -70,7 +70,7 @@ const SignupWindow = (props) => {
             <label htmlFor="pass">Password: </label>
             <input id="pass2" type="password" name="pass2" placeholder="retype password"/>
             <input type="hidden" name="_csrf" value={props.csrf}/>
-            <input className="formSubmit" tpye="submit" value="Sign up"/>
+            <input className="formSubmit" type="submit" value="Sign up"/>
         </form>
     );
 };
