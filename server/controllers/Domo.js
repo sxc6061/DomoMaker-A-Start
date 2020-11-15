973a941bafd330ef944054685bb91f33b0c34cb7
Steven Cho <sxc6061@g.rit.edu>
2020-11-15 18:26:25 -0500

    domomaker d initial commit

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -14,13 +14,14 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req, res) => {
-    if (!req.body.name || !req.body.age) {
+    if (!req.body.name || !req.body.age || !req.body.favFood) {
         return res.status(400).json({ error: 'RAWR! Both name and age are required' });
     }
 
     const domoData = {
         name: req.body.name,
         age: req.body.age,
+        favFood: req.body.favFood,
         owner: req.session.account._id,
     };
 
